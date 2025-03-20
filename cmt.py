8c0e07f120aea891012457da2dad0c5bb22379d4
snitro <snitro@foxmail.com>
2025-03-20 16:24:47 +0800

    update for language

--- cmt.py
+++ cmt.py
@@ -27,7 +27,7 @@ class CMT:
         self.headers = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+            "Accept-Language": "en-US,en;q=0.9,en-GB;q=0.8",
             "Cache-Control": "max-age=0",
             "Connection": "keep-alive",
             "Sec-Fetch-Dest": "document",
